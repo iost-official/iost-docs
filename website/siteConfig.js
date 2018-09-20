061e0462e05d4ff34b3a9b061cef338cfee0aea9
Pana <pan.wang@theseus.app>
2018-09-20 17:57:57 +0800

    update site main color

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -55,7 +55,7 @@ const siteConfig = {
   /* Colors for website */
   colors: {
     // primaryColor: '#2c2e31',
-    primaryColor: '#2e8555',
+    primaryColor: '#2aa3ef',
     secondaryColor: '#205C3B',
   },
   scrollToTop: true,
