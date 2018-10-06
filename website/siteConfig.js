3363c69c239ee3906caa3508462da9df820dbea5
Pana <pan.wang@theseus.app>
2018-10-06 14:18:19 +0800

    enable doc search

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -32,6 +32,11 @@ const siteConfig = {
   // Used for publishing and more
   projectName: 'Iost',
   organizationName: 'IOST',
+  algolia: {
+    apiKey: '31556a769e30bb94f63be6ddcc49f071',
+    indexName: 'iost',
+    algoliaOptions: {} // Optional, if provided by Algolia
+  },
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
