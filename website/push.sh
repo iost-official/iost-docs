ba3b12161fa323a0feddaab8a568a28075de894a
Jack Ji <yuanyi@iost.io>
2019-04-25 12:43:13 +0800

    Add s3 deploy script

--- website/push.sh
+++ website/push.sh
@@ -1,4 +1,9 @@
 set -e
 npm run build
+
+# 推送到文档网站 ec2
 rsync -avzP build/Iost/ ubuntu@iostdoc:/var/www/iost-doc/update/
 ssh ubuntu@iostdoc 'cd /var/www/iost-doc/; bash release.sh'
+
+# 推送到 s3
+#aws --profile s3ops s3 sync build/Iost s3://developers.iost.io
